b34bb2393ed368d4aac36b47c6913e61c370facb
ramalaxmibandi <rlaxmibandi@gmail.com>
2023-11-01 12:55:30 +0000

    my code

---
+++ data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt-get update -y
+apt-get install -y httpd.x86_64
+systemctl start httpd.service
+systemctl enable httpd.service
+echo "Hello World from $(hostname -f)" > /var/www/html/index.html
